ab4d0f6bc93c1cb8e13c175a7d2c42392bdfd353
Duy Tong <dtong92@gmail.com>
2019-04-02 16:24:24 -0700

    working on logging area

--- src/components/DeviceItem.js
+++ src/components/DeviceItem.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default class DeviceItem extends React.Component {
-	render() {
-		return (
-			<div className='item'>
-				<div className='left floated content'>{this.props.device} </div>
-				<div className='right floated content'>
-					<i className='circle check icon green' />{" "}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className='item'>
+        <div className='left floated content'>{this.props.device} </div>
+        <div className='right floated content'>
+          <i className='circle check icon green' />{" "}
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Logging.js
+++ src/components/Logging.js
@@ -1,20 +1,44 @@
 import React from "react";
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 class Logging extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { something: "yes" };
-	}
-	render() {
-		return (
-			<div class='ui form'>
-				<div class='field'>
-					<label>Text</label>
-					<textarea placeholder='Read Only' readonly='' />
-				</div>
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = { something: "" };
+  }
+
+  concatenate = async () => {
+    for (let i = 0; i < 10; i++) {
+      this.setState({
+        something: this.state.something.concat(
+          "LONGGGGGGGGGGGGGGGGG SETTTTTTEEEEEEEEEENCEEEEE"
+        )
+      });
+      console.log(this.state);
+      await sleep(200);
+    }
+  };
+  render() {
+    return (
+      <div class='ui form'>
+        <div class='field'>
+          <label>Text</label>
+          <button className='ui button primary' onClick={this.concatenate}>
+            Concat
+          </button>
+          <textarea
+            id='scroll'
+            value={this.state.something}
+            placeholder='Read Only'
+            readonly=''
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Logging;
